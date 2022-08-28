fdc26bac25aeb040e3a489d04485531048c25bef
kdg1993 <daegunkim0425@gmail.com>
2022-08-28 16:42:40 +0900

    [Model] Add several new strategies * Validation strategy 	* 4 KFold
    * Dataset strategy
    	* fill missing by a single int
    
    * New model
    	* ExtraTreeRegressor

--- main_rf.py
+++ main_rf.py
@@ -11,20 +11,126 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.multioutput import MultiOutputRegressor
 
 # Functions
+def objectiveETR_kfold(trial: Trial, X_tr, y_tr, X_val, y_val):
+    params = {
+        'n_estimators': trial.suggest_categorical('n_estimators', [200]),
+        'max_depth': trial.suggest_int('max_depth', 1, 100),
+        'min_samples_split': trial.suggest_int('min_samples_split', 2, 10),
+        'max_features': trial.suggest_float('max_features', 0.1, 1),
+        'n_jobs': trial.suggest_categorical('n_jobs', [cpu_use]),
+        'random_state': trial.suggest_categorical('random_state', [0]),
+        }
+
+    model = MultiOutputRegressor(
+        ExtraTreesRegressor(**params)
+        )
+
+    # Concat X, y redefine X_tr due to the memory
+    X = pd.concat([X_tr, X_val], axis=0)
+    y = pd.concat([y_tr, y_val], axis=0)
+
+    del X_tr, X_val, y_tr, y_val # due to the memory
+
+    # Calculate KFold mean score
+    kfold_scores = []
+
+    kf = KFold(n_splits=4) # Split 4 because data seems enough and get large val size
+    kf.get_n_splits(X)
+    for tr_idx, val_idx in kf.split(X):
+        X_tr_temp, X_val_temp = X.iloc[tr_idx], X.iloc[val_idx]
+        y_tr_temp, y_val_temp = y.iloc[tr_idx], y.iloc[val_idx]  
+
+        model_fitted = model.fit(X_tr_temp, y_tr_temp)
+
+        r2_temp = r2_score(y_val_temp, model_fitted.predict(X_val_temp))
+        rmse_temp = mean_squared_error(y_val_temp, model_fitted.predict(X_val_temp))**0.5
+    
+        if r2_temp < 0:
+            r2_temp = 1e-10
+
+        score_temp = rmse_temp/r2_temp
+        kfold_scores.append(score_temp)
+
+    return np.mean(kfold_scores)
+
+
+def get_etr_optuna_kfold(X_tr, y_tr, X_val, y_val, n_trials):
+    study = optuna.create_study(
+        study_name='ExtraTreesRegressor_params_opt_by_kfold',
+        direction='minimize',
+        sampler=TPESampler(seed=42),
+        )
+    
+    study.optimize(lambda trial: objectiveETR_kfold(trial, X_tr, y_tr, X_val, y_val),
+                   n_trials=n_trials,
+                   )
+    
+    best_mdl = MultiOutputRegressor(
+        ExtraTreesRegressor(**study.best_params)
+        ).fit(
+        pd.concat([X_tr, X_val], axis=0),
+        pd.concat([y_tr, y_val], axis=0),
+        )
+    
+    return best_mdl, study
+
+
+def objectiveETR(trial: Trial, X_tr, y_tr, X_val, y_val):
+    params = {
+        'n_estimators': trial.suggest_categorical('n_estimators', [200]),
+        'max_depth': trial.suggest_int('max_depth', 1, 100),
+        'min_samples_split': trial.suggest_int('min_samples_split', 2, 10),
+        'max_features': trial.suggest_float('max_features', 0.1, 1),
+        'n_jobs': trial.suggest_categorical('n_jobs', [cpu_use]),
+        'random_state': trial.suggest_categorical('random_state', [0]),
+        }
+
+    model = MultiOutputRegressor(
+        ExtraTreesRegressor(**params)
+        )
+    model_fitted = model.fit(X_tr, y_tr)
+    
+    r2 = r2_score(y_val, model_fitted.predict(X_val))
+    rmse = mean_squared_error(y_val, model_fitted.predict(X_val))**0.5
+    
+    if r2 < 0:
+        r2 = 1e-10
+    
+    return rmse/r2
+
+
+def get_etr_optuna(X_tr, y_tr, X_val, y_val, n_trials):
+    study = optuna.create_study(
+        study_name='ExtraTreesRegressor_params_opt',
+        direction='minimize',
+        sampler=TPESampler(seed=42),
+        )
+    
+    study.optimize(lambda trial: objectiveETR(trial, X_tr, y_tr, X_val, y_val),
+                   n_trials=n_trials,
+                   )
+    
+    best_mdl = MultiOutputRegressor(
+        ExtraTreesRegressor(**study.best_params)
+        ).fit(
+        pd.concat([X_tr, X_val], axis=0),
+        pd.concat([y_tr, y_val], axis=0),
+        )
+    
+    return best_mdl, study
+
+
 def objectiveXGBRF(trial: Trial, X_tr, y_tr, X_val, y_val):
     params = {
         'n_estimators': trial.suggest_categorical('n_estimators', [100]),
         'max_depth': trial.suggest_int('max_depth', 1, 50),
-        'subsample': trial.suggest_float('subsample', 0.1, 1),
+        # 'subsample': trial.suggest_float('subsample', 0.1, 1),
         'colsample_bytree': trial.suggest_float('colsample_bytree', 0.1, 1),
-        'colsample_bynode': trial.suggest_float('colsample_bytree', 0.1, 1),
+        'colsample_bynode': trial.suggest_float('colsample_bynode', 0.1, 1),
         # 'reg_alpha': trial.suggest_float('reg_alpha', 0, 1),
         # 'reg_lambda': trial.suggest_float('reg_lambda', 0, 1),
         'n_jobs': trial.suggest_categorical('n_jobs', [cpu_use]),
         'random_state': trial.suggest_categorical('random_state', [42]),
-        'gpu_id': trial.suggest_categorical('gpu_id', [0]),
-        'verbosity': trial.suggest_categorical('verbosity', [1]),
-        'tree_method': trial.suggest_categorical('tree_method', ['gpu_hist'])
         }
 
     model = MultiOutputRegressor(
@@ -159,7 +265,16 @@ def get_rf_optuna(X_tr, y_tr, X_val, y_val, n_trials):
 
 
 def get_data_and_split(past_step: int, selc_year: list, selc_cols: list,
-                       drop_na_in_X=True):
+                       drop_na_in_X=True, random_seed=42,
+                       na_fill_by_ffill=False, na_fill_limit: int=None, 
+                       na_fill_by_int=False, na_filling_int=None):
+    if na_fill_by_int is True:
+        if na_filling_int is None:
+            raise NotImplementedError('NA fill by int is True but no int is assigned')
+        else:
+            pass
+
+
     if len(selc_year) == 0:
         selc_year = None
    
@@ -168,6 +283,10 @@ def get_data_and_split(past_step: int, selc_year: list, selc_cols: list,
         selc_year=selc_year,
         selc_cols=selc_cols,
         )
+
+    # Fill missing values as int if option is True
+    if na_fill_by_int:
+        X.fillna(na_filling_int, inplace=True)
 
     # Change dtype oftime column in sample submission DataFrame as datetime type
     df_smp_subm = to_datetime_format(df_smp_subm, as_index=False)
@@ -200,7 +319,7 @@ def get_data_and_split(past_step: int, selc_year: list, selc_cols: list,
     X_tr, X_val, y_tr, y_val = train_test_split(
         X, y, 
         test_size=0.2,
-        random_state=seed,
+        random_state=random_seed,
         shuffle=True,
         )
     
@@ -239,8 +358,8 @@ if __name__=='__main__':
     import time
     
     from multiprocessing import cpu_count
-    from sklearn.ensemble import RandomForestRegressor    
-    from sklearn.model_selection import train_test_split
+    from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor    
+    from sklearn.model_selection import train_test_split, KFold
     from optuna.samplers import TPESampler
     from tqdm import tqdm
     from datetime import datetime
@@ -251,7 +370,7 @@ if __name__=='__main__':
     total_run_time_start = time.time()
     
     # Fix random seed
-    fix_random_seed()
+    fix_random_seed(0)
     
     seed = 42
     random.seed(seed)
@@ -259,26 +378,36 @@ if __name__=='__main__':
     os.environ['PYTHONHASHSEED'] = str(seed)
     
     # Configuration
-    cpu_use = round(2*cpu_count()/3)
+    cpu_use = round(1*cpu_count()/2)
     optuna.logging.set_verbosity(1)
     
-    model_nm = 'xgbrf' # rf, lgbm, xgbrf
+    model_nm = 'etr_kfold' # rf, lgbm, xgbrf, etr, xgbrf_na_fill, etr_kfold
     
     save_submission = False
-    print(f'Save submission is [{save_submission}]')
+    print(f'\nSave submission is [{save_submission}]\n')
         
     # Hyperparameter search
     N_TRIALS = 30
    
-    SELC_YEAR = [2012, 2013, 2016, 2017, 2018, 2020, 2022]
+    SELC_YEAR = []#[2012, 2013, 2016, 2017, 2018, 2020, 2022]
     SELC_COLS = [
+        # 'swl',
+        # 'inf', 
+        # 'sfw',
+        # 'ecpc',
+        # 'tototf',
+        # 'tide_level',
         'fw_1018662',
         'fw_1018683',
         'fw_1019630',
         ]    
     DROP_NA_IN_X = False
+    NA_FILL_BY_FFILL = True
+    NA_FILL_LIMIT = None
+    NA_FILL_BY_INT = True
+    NA_FILLING_INT = -9999
     
-    past_step_grid = [15, 18]#, 15, 18, 21, 24]
+    past_step_grid = [6, 12]
     
     df_step_search = pd.DataFrame(
         columns=['model', 'study', 'score'],
@@ -286,14 +415,19 @@ if __name__=='__main__':
         dtype='object',
         )
     df_step_search.index.name = 'backward'
-    df_step_search.score = df_step_search.score.astype('float')
+    df_step_search.score = df_step_search.score.astype('float')        
     
     for step_back in tqdm(past_step_grid):
         X_tr, X_val, y_tr, y_val, X_test = get_data_and_split(
             step_back,
             SELC_YEAR, 
             SELC_COLS,
-            DROP_NA_IN_X,
+            drop_na_in_X=DROP_NA_IN_X,
+            random_seed=seed,
+            na_fill_by_ffill=NA_FILL_BY_FFILL,
+            na_fill_limit=NA_FILL_LIMIT,
+            na_fill_by_int=NA_FILL_BY_INT,
+            na_filling_int=NA_FILLING_INT,
             )
         
         if model_nm == 'rf':
@@ -302,13 +436,17 @@ if __name__=='__main__':
             best_mdl, study = get_lgbm_optuna(X_tr, y_tr, X_val, y_val, N_TRIALS)
         elif model_nm == 'xgbrf':
             best_mdl, study = get_xgbrf_optuna(X_tr, y_tr, X_val, y_val, N_TRIALS)
+        elif model_nm == 'etr':
+            best_mdl, study = get_etr_optuna(X_tr, y_tr, X_val, y_val, N_TRIALS)
+        elif model_nm == 'etr_kfold':
+            best_mdl, study = get_etr_optuna_kfold(X_tr, y_tr, X_val, y_val, N_TRIALS)
         else:
             raise NotImplementedError('Wrong model name to select')
             
         
         df_step_search.loc[step_back, :] = [best_mdl, study, study.best_value]     
         
-    # plot - past time consideration gridsearch scoring result
+    #%% plot - past time consideration gridsearch scoring result
     df_step_search.plot(
         column='score',
         marker='o',
@@ -325,6 +463,12 @@ if __name__=='__main__':
         past_step=best_past_step,
         selc_year=SELC_YEAR,
         selc_cols=SELC_COLS,
+        drop_na_in_X=DROP_NA_IN_X,
+        random_seed=seed,
+        na_fill_by_ffill=NA_FILL_BY_FFILL,
+        na_fill_limit=NA_FILL_LIMIT,
+        na_fill_by_int=NA_FILL_BY_INT,
+        na_filling_int=NA_FILLING_INT,
         )
 
     # Change dtype oftime column in sample submission DataFrame as datetime type
@@ -354,10 +498,11 @@ if __name__=='__main__':
             pass
         
         # Save model and prediction csv
+        df_smp_subm.to_csv(save_path+'/submission.csv', index=None)
+        
         with open(save_path+'/model', 'wb') as f:
             pickle.dump(mdl_best, f)
             
-        df_smp_subm.to_csv(save_path+'/submission.csv', index=None)
     
     # Print total time
     print(f'Total run time [{(time.time()-total_run_time_start)/60:.1f}]min')
