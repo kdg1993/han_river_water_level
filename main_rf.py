2fced20b7047d0abe137506f577901c2f1288c8a
kdg1993 <daegunkim0425@gmail.com>
2022-08-30 16:39:58 +0900

    [Model] Add RandomForest KFold strategy

--- main_rf.py
+++ main_rf.py
@@ -11,14 +11,80 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.multioutput import MultiOutputRegressor
 
 # Functions
+def objectiveRF_kfold(trial: Trial, X_tr, y_tr, X_val, y_val):
+    params = {
+        'n_estimators': trial.suggest_categorical('n_estimators', [100]),
+        'max_depth': trial.suggest_int('max_depth', 1, 100),
+        'min_samples_split': trial.suggest_int('min_samples_split', 2, 20),
+        'max_features': trial.suggest_float('max_features', 0.3, 1),
+        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 20),
+        'n_jobs': trial.suggest_categorical('n_jobs', [cpu_use]),
+        'random_state': trial.suggest_categorical('random_state', [42]),
+        }
+
+    model = MultiOutputRegressor(
+        RandomForestRegressor(**params)
+        )
+
+    # Concat X, y redefine X_tr due to the memory
+    X = pd.concat([X_tr, X_val], axis=0)
+    y = pd.concat([y_tr, y_val], axis=0)
+
+    del X_tr, X_val, y_tr, y_val # due to the memory
+
+    # Calculate KFold mean score
+    kfold_scores = []
+
+    kf = KFold(n_splits=4) # Split 4 because data seems enough and get large val size
+    kf.get_n_splits(X)
+    for tr_idx, val_idx in kf.split(X):
+        X_tr_temp, X_val_temp = X.iloc[tr_idx], X.iloc[val_idx]
+        y_tr_temp, y_val_temp = y.iloc[tr_idx], y.iloc[val_idx]  
+
+        model_fitted = model.fit(X_tr_temp, y_tr_temp)
+
+        r2_temp = r2_score(y_val_temp, model_fitted.predict(X_val_temp))
+        rmse_temp = mean_squared_error(y_val_temp, model_fitted.predict(X_val_temp))**0.5
+    
+        if r2_temp < 0:
+            r2_temp = 1e-10
+
+        score_temp = rmse_temp/r2_temp
+        kfold_scores.append(score_temp)
+
+    return np.mean(kfold_scores)
+
+
+def get_rf_optuna_kfold(X_tr, y_tr, X_val, y_val, n_trials):
+    study = optuna.create_study(
+        study_name='RandomForestRegressor_params_opt_by_kfold',
+        direction='minimize',
+        sampler=TPESampler(seed=42),
+        )
+    
+    study.optimize(lambda trial: objectiveRF_kfold(trial, X_tr, y_tr, X_val, y_val),
+                   n_trials=n_trials,
+                   )
+    
+    best_mdl = MultiOutputRegressor(
+        RandomForestRegressor(**study.best_params)
+        ).fit(
+        pd.concat([X_tr, X_val], axis=0),
+        pd.concat([y_tr, y_val], axis=0),
+        )
+    
+    return best_mdl, study
+
+
 def objectiveETR_kfold(trial: Trial, X_tr, y_tr, X_val, y_val):
     params = {
-        'n_estimators': trial.suggest_categorical('n_estimators', [200]),
+        'n_estimators': trial.suggest_categorical('n_estimators', [100]),
         'max_depth': trial.suggest_int('max_depth', 1, 100),
         'min_samples_split': trial.suggest_int('min_samples_split', 2, 10),
-        'max_features': trial.suggest_float('max_features', 0.1, 1),
+        'max_features': trial.suggest_float('max_features', 0.3, 1),
+        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 20),
         'n_jobs': trial.suggest_categorical('n_jobs', [cpu_use]),
-        'random_state': trial.suggest_categorical('random_state', [0]),
+        'random_state': trial.suggest_categorical('random_state', [42]),
         }
 
     model = MultiOutputRegressor(
@@ -82,7 +148,7 @@ def objectiveETR(trial: Trial, X_tr, y_tr, X_val, y_val):
         'min_samples_split': trial.suggest_int('min_samples_split', 2, 10),
         'max_features': trial.suggest_float('max_features', 0.1, 1),
         'n_jobs': trial.suggest_categorical('n_jobs', [cpu_use]),
-        'random_state': trial.suggest_categorical('random_state', [0]),
+        'random_state': trial.suggest_categorical('random_state', [42]),
         }
 
     model = MultiOutputRegressor(
@@ -319,7 +385,7 @@ def get_data_and_split(past_step: int, selc_year: list, selc_cols: list,
     X_tr, X_val, y_tr, y_val = train_test_split(
         X, y, 
         test_size=0.2,
-        random_state=random_seed,
+        random_state=seed,
         shuffle=True,
         )
     
@@ -370,7 +436,7 @@ if __name__=='__main__':
     total_run_time_start = time.time()
     
     # Fix random seed
-    fix_random_seed(0)
+    fix_random_seed()
     
     seed = 42
     random.seed(seed)
@@ -378,10 +444,10 @@ if __name__=='__main__':
     os.environ['PYTHONHASHSEED'] = str(seed)
     
     # Configuration
-    cpu_use = round(1*cpu_count()/2)
-    optuna.logging.set_verbosity(1)
+    cpu_use = round(2*cpu_count()/3)
+    optuna.logging.set_verbosity(2)
     
-    model_nm = 'etr_kfold' # rf, lgbm, xgbrf, etr, xgbrf_na_fill, etr_kfold
+    model_nm = 'etr_kfold' # rf, lgbm, xgbrf, etr, xgbrf_na_fill, etr_kfold, rf_kfold
     
     save_submission = False
     print(f'\nSave submission is [{save_submission}]\n')
@@ -389,14 +455,14 @@ if __name__=='__main__':
     # Hyperparameter search
     N_TRIALS = 30
    
-    SELC_YEAR = []#[2012, 2013, 2016, 2017, 2018, 2020, 2022]
+    SELC_YEAR = [2012, 2013, 2016, 2017, 2018, 2020, 2022]
     SELC_COLS = [
-        # 'swl',
-        # 'inf', 
-        # 'sfw',
-        # 'ecpc',
-        # 'tototf',
-        # 'tide_level',
+        'swl',
+        'inf', 
+        'sfw',
+        'ecpc',
+        'tototf',
+        'tide_level',
         'fw_1018662',
         'fw_1018683',
         'fw_1019630',
@@ -407,7 +473,7 @@ if __name__=='__main__':
     NA_FILL_BY_INT = True
     NA_FILLING_INT = -9999
     
-    past_step_grid = [6, 12]
+    past_step_grid = [6]
     
     df_step_search = pd.DataFrame(
         columns=['model', 'study', 'score'],
@@ -440,6 +506,8 @@ if __name__=='__main__':
             best_mdl, study = get_etr_optuna(X_tr, y_tr, X_val, y_val, N_TRIALS)
         elif model_nm == 'etr_kfold':
             best_mdl, study = get_etr_optuna_kfold(X_tr, y_tr, X_val, y_val, N_TRIALS)
+        elif model_nm == 'rf_kfold':
+            best_mdl, study = get_rf_optuna_kfold(X_tr, y_tr, X_val, y_val, N_TRIALS)
         else:
             raise NotImplementedError('Wrong model name to select')
             
